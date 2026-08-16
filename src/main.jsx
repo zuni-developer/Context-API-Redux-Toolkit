4d7a415c8fd90b900f825b2721f79af3ee1dc160
zuni-developer <zunairasabir7@gmail.com>
2026-08-16 21:32:44 +0500

    Add app entry point and base styles

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,17 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { Provider } from "react-redux"; // Redux Toolkit
+import { store } from "./redux/store";
+import { ThemeProvider } from "./context/ThemeContext"; // Context API
+import App from "./App.jsx";
+import "./index.css";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <Provider store={store}>
+      <ThemeProvider>
+        <App />
+      </ThemeProvider>
+    </Provider>
+  </StrictMode>
+);
